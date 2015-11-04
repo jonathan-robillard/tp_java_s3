a58e6025dc4ef8f3b5053c6036d28921b98c4686
jonathan-robillard <jonathan.robillard@u-psud.fr>
2015-11-04 09:07:21 +0100

    Initialisation

---
+++ src/edu/iut/app/IApplicationLog.java
@@ -0,0 +1,19 @@
+package edu.iut.app;
+
+public interface IApplicationLog {
+	/** TP1 : Créérr les méthodes :
+	 - setMessage
+	  - getMessage
+	  - addListener
+	  - getApplicationLogListeners();
+	*/
+    
+    public void setMessage(String message);
+    
+    public String getMessage();
+ 
+    public void addListener(IApplicationLogListener listener);
+    
+    public IApplicationLogListener[] getpplicationLogListeners();
+
+}
---
+++ src/edu/iut/app/IApplicationLogListener.java
@@ -0,0 +1,10 @@
+package edu.iut.app;
+
+public interface IApplicationLogListener {
+	/** TP1 : Créer la fuonction newMessage */
+    
+    public void newMessage(String level, String message)
+    {
+        
+    }
+}
