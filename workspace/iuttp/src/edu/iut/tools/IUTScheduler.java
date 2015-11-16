a024eaedc8e8c65e1a2d1ee9165b7dfb5b8013d1
François Rousselet <contact@francoisrousselet.fr>
2015-11-16 14:18:50 +0100

    test

--- workspace/iuttp/src/edu/iut/tools/IUTScheduler.java
+++ workspace/iuttp/src/edu/iut/tools/IUTScheduler.java
@@ -3,13 +3,14 @@ package edu.iut.tools;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+// Test
 
 public class IUTScheduler {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 		    public void run() {
 		        JFrame mainFrame = new edu.iut.gui.frames.SchedulerFrame("IUT Scheduler");
-		        mainFrame.setVisible(true);		        
+		        mainFrame.setVisible(true);    
 		    }
 		});
 	}
