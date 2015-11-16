4157634d1dcf244a8eaa40059298078b8d20abd0
jonathan-robillard <jonathan.robillard@u-psud.fr>
2015-11-16 10:24:31 +0100

    finalisation exo2 TP0

--- workspace/iuttp/src/edu/iut/app/ApplicationLogs.java
+++ workspace/iuttp/src/edu/iut/app/ApplicationLogs.java
@@ -11,15 +11,30 @@ public class ApplicationLogs extends ArrayList<IApplicationLog> {
 	public ArrayList<IApplicationLog> getErrors() {
 		ArrayList<IApplicationLog> filteredLogs = new ArrayList<IApplicationLog>();
 		// une boucle à faire ici, penser à utiliser instanceof
+		for(int i = 0; i<filteredLogs.size(); i++)
+		{
+			filteredLogs.get(i).getMessage();
+		}
+		
 		return filteredLogs;
 	}
 	public ArrayList<IApplicationLog> getWarnings() {
 		ArrayList<IApplicationLog> filteredLogs = new ArrayList<IApplicationLog>();
 		// une boucle à faire ici, penser à utiliser instanceof
+		for(int i = 0; i<filteredLogs.size(); i++)
+		{
+			filteredLogs.get(i).getMessage();
+		}
+		
 		return filteredLogs;
 	}
 	public ArrayList<IApplicationLog> getInfos() {
 		ArrayList<IApplicationLog> filteredLogs = new ArrayList<IApplicationLog>();
+		
+		for(int i = 0; i<filteredLogs.size(); i++)
+		{
+			filteredLogs.get(i).getMessage();
+		}
 		// une boucle à faire ici, penser à utiliser instanceof
 		return filteredLogs;
 	}
