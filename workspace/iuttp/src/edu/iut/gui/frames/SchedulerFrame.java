90861bd787d036ce0e1f0e1c68a58dec3a7f538f
François Rousselet <Franois Rousselet>
2015-11-23 11:08:44 +0100

    Ajout menu

--- workspace/iuttp/src/edu/iut/gui/frames/SchedulerFrame.java
+++ workspace/iuttp/src/edu/iut/gui/frames/SchedulerFrame.java
@@ -17,6 +17,8 @@ import javax.swing.JSplitPane;
 import edu.iut.gui.widget.agenda.AgendaPanelFactory;
 import edu.iut.gui.widget.agenda.ControlAgendaViewPanel;
 import edu.iut.gui.widget.agenda.AgendaPanelFactory.ActiveView;
+import java.awt.event.KeyEvent;
+import javax.swing.KeyStroke;
 
 
 public class SchedulerFrame extends JFrame {
@@ -46,15 +48,62 @@ public class SchedulerFrame extends JFrame {
 		this.setContentPane(splitPane);
 		
 		JMenuBar menuBar = new JMenuBar();
-		JMenu menu;		
-		JMenuItem menuItem;
+		JMenu menuFile;
+                JMenu menuEdit;
+                JMenu menuHelp;
+		JMenuItem miLoad;
+                JMenuItem miSave;
+                JMenuItem miQuit;
+                JMenuItem miView;
+                JMenuItem miDisplay;
+                JMenuItem miAbout;
 		
 		/* File Menu */
 		/** EX4 : MENU : UTILISER L'AIDE FOURNIE DANS LE TP**/
 		
-		
-		menu = new JMenu("File");
-		
+		menuFile = new JMenu("File");
+                menuEdit = new JMenu("Edit");
+                menuHelp = new JMenu("Help");
+		menuBar.add(menuFile);
+                menuBar.add(menuEdit);
+                menuBar.add(menuHelp);
+                
+                
+                
+                menuFile.setMnemonic(KeyEvent.VK_A);
+                menuFile.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
+                menuBar.add(menuFile);
+                menuBar.add(menuEdit);
+                menuBar.add(menuHelp);
+                
+                
+                miLoad = new JMenuItem("Load", KeyEvent.VK_T);
+                miLoad.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+                miLoad.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
+                menuFile.add(miLoad);
+                
+                miSave = new JMenuItem("Save", KeyEvent.VK_T);
+                miSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+                miSave.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
+                menuFile.add(miSave);
+                
+                miQuit = new JMenuItem("Quit", KeyEvent.VK_T);
+                miQuit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+                miQuit.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
+                menuFile.add(miQuit);
+                
+                miView = new JMenuItem("View", KeyEvent.VK_T);
+                miView.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+                miView.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
+                menuEdit.add(miView);
+                
+                miAbout = new JMenuItem("About", KeyEvent.VK_T);
+                miAbout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+                miAbout.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
+                menuHelp.add(miAbout);
+                
+                
+                // contentPane.add(menuBar);
 		
 		this.setJMenuBar(menuBar);
 		this.pack();
