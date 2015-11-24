274d81b3b783029a79ee83bb6d4249f7b0a7e7ae
François Rousselet <contact@francoisrousselet.fr>
2015-11-24 13:47:32 +0100

    (temp)

--- workspace/iuttp/src/edu/iut/gui/widget/agenda/ControlAgendaViewPanel.java
+++ workspace/iuttp/src/edu/iut/gui/widget/agenda/ControlAgendaViewPanel.java
@@ -15,6 +15,7 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.SpinnerModel;
 
 import edu.iut.app.ApplicationSession;
+import javax.swing.SpinnerListModel;
 
 public class ControlAgendaViewPanel extends JPanel {
 
@@ -30,6 +31,24 @@ public class ControlAgendaViewPanel extends JPanel {
 		this.agendaViewLayout = layerLayout;
 		this.contentPane = contentPane;
 		/** EX3: REMPLACEMENT DU BOUTON NEXT */
+                String[] year = null;
+                for( int i=0 ; i<10 ; i++) {
+                    int y = 2010 + i;
+                    year[i] = toString(y);
+                }
+                SpinnerListModel monthModel = new SpinnerListModel(monthStrings);
+                JSpinner spinner = new JSpinner(monthModel);
+                
+                //Spinner nextView = new JButton("Next");
+		nextView.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				agendaViewLayout.next(contentPane);				
+			}			
+		});
+		this.add(nextView);
+                
 	}
 	
 	public int getYear() {
